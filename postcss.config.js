054d987fa3e7f85e67a361aac715df09e4ce03f0
Emerson MX <emersonmx@gmail.com>
2020-01-09 21:32:45 -0300

    Update configs

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,13 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
     plugins: [
-        require("postcss-import"),
-        require("postcss-simple-vars"),
-        require("tailwindcss"),
-        require("autoprefixer")
-    ]
+        require('postcss-import'),
+        require('postcss-simple-vars'),
+        require('tailwindcss'),
+        require('autoprefixer'),
+        purgecss({
+            content: ['./dist/**/*.html'],
+        }),
+    ],
 };
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
 import _ from 'lodash';
+import axios from 'axios';
 
 import './main.css';
