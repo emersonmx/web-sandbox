da277c6e904366d3a6869c640674e4498613c711
Emerson MX <emersonmx@gmail.com>
2020-01-10 09:37:59 -0300

    Finish pure webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,30 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 
+const ManifestPlugin = require('webpack-manifest-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const nodeEnv = process.env.NODE_ENV;
-const isProduction = nodeEnv == 'production';
+const webpackNotifierPlugin = new WebpackNotifierPlugin({ alwaysNotify: true });
+const cleanWebpackPlugin = new CleanWebpackPlugin({
+    cleanOnceBeforeBuildPatterns: ['main.*', 'manifest.json'],
+});
 
-commonConfig = {
+const isProduction = process.env.NODE_ENV == 'production';
+
+const commonConfig = {
     entry: './src/main.js',
     output: {
-        filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-        new MiniCssExtractPlugin({ filename: 'main.css' }),
-        new WebpackNotifierPlugin({ alwaysNotify: true }),
+        new webpack.ProgressPlugin(),
+        cleanWebpackPlugin,
+        webpackNotifierPlugin,
     ],
     module: {
         rules: [
@@ -37,16 +46,24 @@ const devConfig = merge(commonConfig, {
     devServer: {
         contentBase: './dist',
     },
+    output: {
+        filename: 'main.js',
+    },
+    plugins: [new MiniCssExtractPlugin({ filename: 'main.css' })],
 });
-
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
 
 const prodConfig = merge(commonConfig, {
     mode: 'production',
     optimization: {
         minimizer: [new TerserPlugin(), new OptimizeCSSAssetsPlugin({})],
     },
+    output: {
+        filename: 'main.[contenthash].js',
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ filename: 'main.[contenthash].css' }),
+        new ManifestPlugin(),
+    ],
 });
 
 module.exports = isProduction ? prodConfig : devConfig;
