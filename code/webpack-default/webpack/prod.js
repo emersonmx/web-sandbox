c6110ec17795b314f561b7c5d0677d5a59240468
Emerson MX <emersonmx@gmail.com>
2020-03-24 18:57:41 -0300

    Organize webpack

---
+++ code/webpack-default/webpack/prod.js
@@ -0,0 +1,30 @@
+const merge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const common = require('./common');
+
+module.exports = merge(common, {
+    mode: 'production',
+
+    output: {
+        filename: '[name].js?[contenthash]',
+        chunkFilename: '[id].css?[contenthash]',
+    },
+
+    optimization: {
+        minimizer: [
+            new TerserPlugin({
+                sourceMap: true,
+            }),
+
+            new MiniCssExtractPlugin({
+                filename: '[name].[hash:5].css',
+                chunkFilename: '[id].[hash:5].css',
+            }),
+
+            new OptimizeCSSAssetsPlugin({}),
+        ],
+    },
+});
